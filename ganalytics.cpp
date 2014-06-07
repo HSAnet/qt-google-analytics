28d6d9b3fa392079d332ab5e2b36b0a53b303acf
Weiss Christian <weiss-christian@hotmail.de>
2014-06-07 19:26:04 +0200

    Get operating system on windows

--- ganalytics.cpp
+++ ganalytics.cpp
@@ -278,10 +278,57 @@ QString GAnalytics::getSystemInfo()
     return os;
 }
 #endif
-/*
+
 #ifdef Q_OS_WIN
+/**
+ * Only on Windows
+ * Get operating system and its version.
+ * @return os   A QString containing the oprating systems name and version.
+ */
+QString GAnalytics::getSystemInfo()
+{
+    QSysInfo::WindowsVersion version = QSysInfo::windowsVersion();
+    QString os("Windows; ");
+    switch (version) {
+    case QSysInfo::WV_95:
+        os += "Win 95";
+        break;
+    case QSysInfo::WV_98:
+        os += "Win 98";
+        break;
+    case QSysInfo::WV_Me:
+        os += "Win ME";
+        break;
+    case QSysInfo::WV_NT:
+        os += "Win NT";
+        break;
+    case QSysInfo::WV_2000:
+        os += "Win 2000";
+        break;
+    case QSysInfo::WV_2003:
+        os += "Win Server 2003";
+        break;
+    case QSysInfo::WV_VISTA:
+        os += "Win Vista";
+        break;
+    case QSysInfo::WV_WINDOWS7:
+        os += "Win 7";
+        break;
+    case QSysInfo::WV_WINDOWS8:
+        os += "Win 8";
+        break;
+    case QSysInfo::WV_WINDOWS8_1:
+        os += "Win 8.1";
+        break;
+    default:
+        os = "Windows; unknown";
+        break;
+    }
+    return os;
+}
 #endif
 
+/*
 #ifdef Q_OS_LINUX
 #endif
 */
